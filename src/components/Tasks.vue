c1d8dc76b33059504314d9751768e9ba39d509cc
jmcob <jmcob@pm.me>
2022-01-19 10:31:39 +0100

    update

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+        <div>
+                <div  v-for="task in tasks" :key="task.id">
+                        <Task :task="task" />
+                </div>
+        </div>
+</template>
+
+<script>
+import Task from './Task'
+export default {
+        name: "Tasks",
+        props: {
+                tasks: Array
+        },
+        components: {Task,},
+}
+</script>
