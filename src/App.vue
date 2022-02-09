df17b0746174ad86be0a51dc3b9dc232f5411033
jmcob <jmcob@pm.me>
2022-02-09 16:03:18 +0100

    front ok, back bug en delete

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,24 @@
                         title="Task Tracker"
                         :showAddTask="showAddTask"
                 />
-                <div v-if="showAddTask">
-                        <AddTask @add-task="addTask" />
-                </div>
-                <Tasks
-                        @toggle-reminder="toggleReminder"
-                        @delete-task="deleteTask"
-                        :tasks="tasks"
-                />
+
+                <router-view :showAddTask="showAddTask"></router-view>
+                <Footer />
         </div>
 </template>
 
 <script>
 import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
 
 export default {
         name: "App",
         components: {
                 Header,
-                Tasks,
-                AddTask,
+                Footer,
         },
         data() {
                 return {
-                        tasks: [],
                         showAddTask: false,
                 };
         },
@@ -38,79 +30,6 @@ export default {
                 toggleAddTask() {
                         this.showAddTask = !this.showAddTask;
                 },
-                async addTask(newTask) {
-                        let task = JSON.stringify(newTask);
-                        const res = await fetch(
-                                process.env.VUE_APP_API_URL + "/api/tasks/",
-                                {
-                                        method: "POST",
-                                        headers: {
-                                                "Content-type":
-                                                        "application/json",
-                                        },
-                                        body: task,
-                                }
-                        );
-                        const data = await res.json();
-                        this.tasks = [...this.tasks, data];
-                },
-                async deleteTask(id) {
-                        const res = await fetch(
-                                process.env.VUE_APP_API_URL +
-                                        `/api/tasks/${id}`,
-                                {
-                                        method: "DELETE",
-                                }
-                        );
-
-                        res.status === 200
-                                ? (this.tasks = this.tasks.filter(
-                                          (task) => task._id !== id
-                                  ))
-                                : alert("Error deleting task");
-                },
-                async toggleReminder(id) {
-                        const taskToToggle = await this.fetchTask(id);
-                        const updTask = {
-                                ...taskToToggle,
-                                reminder: !taskToToggle.reminder,
-                        };
-                        const res = await fetch(
-                                process.env.VUE_APP_API_URL +
-                                        `/api/tasks/${id}`,
-                                {
-                                        method: "PUT",
-                                        headers: {
-                                                "Content-type":
-                                                        "application/json",
-                                        },
-                                        body: JSON.stringify(updTask),
-                                }
-                        );
-                        const data = await res.json();
-                        this.tasks = this.tasks.map((task) =>
-                                task._id === data._id
-                                        ? { ...task, reminder: data.reminder }
-                                        : task
-                        );
-                },
-                async fetchTasks() {
-                        const res = await fetch(
-                                process.env.VUE_APP_API_URL + "/api/tasks"
-                        );
-                        const data = await res.json();
-                        return data;
-                },
-                async fetchTask(id) {
-                        const res = await fetch(
-                                process.env.VUE_APP_API_URL + `/api/tasks/${id}`
-                        );
-                        const data = await res.json();
-                        return data;
-                },
-        },
-        async created() {
-                this.tasks = await this.fetchTasks();
         },
 };
 </script>
---
+++ src/views/About.vue
@@ -0,0 +1,4 @@
+<template>
+        <h2>Version 1.0.0</h2>
+        <router-link to="/">Go back</router-link>
+</template>
